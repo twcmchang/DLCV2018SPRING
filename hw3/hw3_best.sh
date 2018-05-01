662ad1ae3d3bf3f11769f8f61f2ac410eab0ce38
twcmchang <twcmchang@gmail.com>
2018-05-01 21:15:48 +0800

    [Update] bash hw3_best.sh <test_dir> <plot_dir>

--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O save_8s.zip https://www.dropbox.com/s/eaiqzffmczgq13m/save_8s.zip?dl=0
-unzip save_8s.zip save_best/
-python3 test.py --test_dir $1 --plot_dir $2 --init_from save_best/keras_vgg16.npy --save_dir save_best/
+unzip save_8s.zip -d save_best/
+python3 test.py --test_dir $1 --plot_dir $2 --mode FCN8s --init_from save_best/keras-vgg16.npy --save_dir save_best/
