908e7cdef4bf361187c948e82b160806fd186247
cmchang <twcmchang@gmail.com>
2018-05-17 09:00:56 -0400

    [Update] dropbox links

--- hw4/hw4.sh
+++ hw4/hw4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget -O vae.zip "https://www.dropbox.com/s/1paw83y9jyednh4/save_v2_ep1k.zip?dl=0"
+wget -O vae.zip "https://www.dropbox.com/s/efcl6le9io9tpgj/save_vae.zip?dl=0"
 unzip vae.zip -d save_vae/
-wget -O gan.zip "https://www.dropbox.com/s/1paw83y9jyednh4/save_v2_ep1k.zip?dl=0"
+wget -O gan.zip "https://www.dropbox.com/s/0tzn51x72s3tue0/save_gan.zip?dl=0"
 unzip gan.zip -d save_gan/
-wget -O acgan.zip "https://www.dropbox.com/s/1paw83y9jyednh4/save_v2_ep1k.zip?dl=0"
+wget -O acgan.zip "https://www.dropbox.com/s/ku965cvmc0n6xs0/save_acgan.zip?dl=0"
 unzip acgan.zip -d save_acgan/
 python3 test_vae.py --save_dir save_vae/ --data_dir $1 --plot_dir $2
 python3 test_gan.py --save_dir save_gan/ --plot_dir $2
