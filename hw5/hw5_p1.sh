39fa6d161c554e8392f8489a99f365fcbf64e08d
cmchang <twcmchang@gmail.com>
2018-06-10 23:24:51 +0800

    [update] add specific file location

--- hw5/hw5_p1.sh
+++ hw5/hw5_p1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -O save_Q1.zip https://www.dropbox.com/s/q97xiqzcvbazeq5/save_32s.zip?dl=0
 unzip save_Q1.zip -d save_Q1/
-python3 extractor.py --video_path $1 --video_list $2 --filename valid_codes_full.pkl
-python3 test_Q1.py --valid_video_list $2 --output_dir $3
+python3 extractor.py --video_path $1 --video_list $2 --filename valid_codes_full.pkl --save_dir save
+python3 test_Q1.py --valid_video_list $2 --output_dir $3 --valid_pkl_file save/valid_codes_full.pkl
