b049ee74b2d592bae42312b6f901c4021aadfd5f
twcmchang <twcmchang@gmail.com>
2018-06-11 10:15:14 +0800

    [update] fix typo

--- hw5/hw5_p1.sh
+++ hw5/hw5_p1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O updated_kears_vgg16.npy https://www.dropbox.com/s/e4kifd7wpayxyzj/updated_keras_vgg16.npy?dl=0
+wget -O updated_keras_vgg16.npy https://www.dropbox.com/s/e4kifd7wpayxyzj/updated_keras_vgg16.npy?dl=0
 wget -O save_Q1.zip https://www.dropbox.com/s/w32h5qkqvs3xs7a/save_Q1.zip?dl=0
 unzip save_Q1.zip -d save_Q1/
 python3 extractor.py --video_path $1 --video_list $2 --filename valid_codes_full.pkl --save_dir save
--- hw5/hw5_p2.sh
+++ hw5/hw5_p2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O updated_kears_vgg16.npy https://www.dropbox.com/s/e4kifd7wpayxyzj/updated_keras_vgg16.npy?dl=0
+wget -O updated_keras_vgg16.npy https://www.dropbox.com/s/e4kifd7wpayxyzj/updated_keras_vgg16.npy?dl=0
 wget -O save_Q2.zip https://www.dropbox.com/s/mj7tr4i8pg2hxc6/save_Q2.zip?dl=0
 unzip save_Q2.zip -d save_Q2/
 python3 extractor.py --video_path $1 --video_list $2 --filename valid_codes_full.pkl --save_dir save
